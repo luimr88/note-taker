c742ff0ba198f0cdeeec131db5978c5cc1ee69c4
Luis <luisriveros26@gmail.com>
2022-05-28 18:10:42 -0600

    code added to api routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const {
     createNewNote,
+    validateNote,
 } = require("../../lib/notes");
+const { notes } = require("../../db/db.json");
+
+router.post("/notes", (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send("The note is not properly formatted.");
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+})
+
+
 
 module.exports = router;
