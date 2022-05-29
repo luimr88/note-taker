2384fdf260ba6afb73ef3c1cb3ecf28114e5e4df
Luis <luisriveros26@gmail.com>
2022-05-29 17:43:03 -0600

    updated code

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
+const fs = require("fs");
 const {
     createNewNote,
     validateNote,
 } = require("../../lib/notes");
 const { notes } = require("../../db/db.json");
+
+router.get("/api/notes", (req, res) => {
+    fs.readFile("../../db/db.json", (err, data) => {
+        if(err) {
+            res.sendStatus(404);
+        } else {
+            console.log(data);
+            res.json(notes);
+        }
+    })
+})
 
 router.post("/notes", (req, res) => {
     req.body.id = notes.length.toString();
