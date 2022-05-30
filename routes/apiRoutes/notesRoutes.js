2ef6a6492405541c4732ccff15fa46a9d867f687
Luis Riveros <luisriveros26@gmail.com>
2022-05-30 00:07:27 -0600

    added delete method

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,21 +1,26 @@
 const router = require("express").Router();
+const path = require("path");
 const fs = require("fs");
 const {
     createNewNote,
     validateNote,
+    findById,
 } = require("../../lib/notes");
-const { notes } = require("../../db/db.json");
+const { notes } = require("../../db/db");
 
-router.get("/api/notes", (req, res) => {
-    fs.readFile("../../db/db.json", (err, data) => {
-        if(err) {
-            res.sendStatus(404);
-        } else {
-            console.log(data);
-            res.json(notes);
-        }
-    })
-})
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+});
+
+router.get("/notes/:id", (req, res) => {
+    const result = findById(req.params.id, notes);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
 router.post("/notes", (req, res) => {
     req.body.id = notes.length.toString();
@@ -26,8 +31,20 @@ router.post("/notes", (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note);
     }
-})
+});
 
+router.delete("/notes/:id", (req, res) => {
+    for (let i = 0; i < notes.length; i++) {
+        if (notes[i].id === req.params.id) {
+            notes.splice(i, 1);
+        }
+    }
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(notes, null, 2)
+    );
+    res.json(notes);
+})
 
 
 module.exports = router;
